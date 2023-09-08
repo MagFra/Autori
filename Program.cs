9424a5a2b3d8fda050a647657acc19b5673c08a0
Magnus Fransson <magnus.fransson.66@bahnhof.se>
2023-09-08 17:04:52 +0200

    First itteration!

--- Program.cs
+++ Program.cs
@@ -1,10 +1,29 @@
-﻿namespace Autori
+﻿using System;
+
+namespace Autori
 {
     internal class Program
     {
+#pragma warning disable CS8600 // Converting null literal or possible null value to non-nullable type.
+#pragma warning disable CS8602 // Dereference of a possibly null reference.
+#pragma warning disable CS8604 // Possible null reference argument.
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            System.Text.StringBuilder line = new System.Text.StringBuilder( Console.ReadLine()); string result = "";
+            //line = line.Trim();
+            result += line[1];
+            for (int i = 0; i < line.Length; i++)
+            {
+                if (line[i] == '-') { result += line[i + 1]; }
+            }
+            System.Console.WriteLine(result);
         }
+
+
+#pragma warning restore CS8604 // Possible null reference argument.
+#pragma warning restore CS8602 // Dereference of a possibly null reference.
+#pragma warning restore CS8600 // Converting null literal or possible null value to non-nullable type.
     }
 }
